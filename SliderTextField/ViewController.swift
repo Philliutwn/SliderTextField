b7a44df62662ff7ead08f98eab587515bd5e4cc1
劉鴻祥 <phil@liuhongxiangde-macbook-pro.local>
2018-07-22 15:08:44 +0800

    Initial release

--- SliderTextField/ViewController.swift
+++ SliderTextField/ViewController.swift
@@ -10,6 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var fontSize : Float = 0.0
+    
+    @IBOutlet weak var fontSizeSlider: UISlider!
+    @IBOutlet weak var fontLenghLabel: UILabel!
+    @IBOutlet weak var fontSizeLabel: UILabel!
+    @IBOutlet weak var displayTextField: UITextField!
+    @IBAction func fontSizeSliderMoved(_ sender: UISlider) {
+        
+        let fontName = displayTextField.font!
+        fontSize = sender.value
+        fontSizeLabel.text = String (format: "%.2f",fontSize)
+        //print ("Slider value=",fontSize)
+        //print (fontSizeLabel.text)
+        displayTextField.font = fontName.withSize(CGFloat(fontSize))
+        fontLenghLabel.text =  String (displayTextField.text!.count)
+    }
+    
+    @IBAction func entryTextField(_ sender: UITextField) {
+        
+        fontSizeLabel.text = String (fontSizeSlider.value)
+        fontLenghLabel.text =  String (displayTextField.text!.count)
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
